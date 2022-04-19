a415fe9dcf376c9ecc1bdf230db102d94b0d508b
Blossom <saphashb@gmail.com>
2022-04-19 15:09:14 +0100

    write tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,13 @@
-const { ord, ordFrom } = require('./index')
+const { ord, ordFrom } = require('../index')
 
-describe('#index')
-console.log(ord('a'))
-console.log(ordFrom('578'))
+describe('#index', () => {
+  it('expects the ascii value', () => {
+    expect(ord('a')).toBe(97)
+    expect(ord('A')).toBe(65)
+  })
+  it('expects the character of ascii value', () => {
+    expect(ordFrom(65)).toBe('A')
+    expect(ordFrom(97)).toBe('a')
+  })
+})
+
